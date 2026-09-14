edf0163432bdda47c1fdb83ea07e3a6928dfd374
Assem <assem.bakr@gmail.com>
2026-09-14 22:09:35 +0300

    Refactor CSS variables for dark mode support; implement theme toggle functionality in JavaScript

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 
+const themebutton = document.getElementById("theme-toggle")
+const htmlElement = document.documentElement;
+
+
+themebutton.addEventListener("click", () => {
+    const currentTheme = htmlElement.getAttribute("data-theme");
+
+    if (currentTheme === "light"){
+        htmlElement.setAttribute("data-theme", "dark");
+    } else {
+        htmlElement.setAttribute("data-theme", "light");
+    }
+
+});
